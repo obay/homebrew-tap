07dffa244e8ce840ed5794d3f2b4acd246775631
goreleaserbot <goreleaser@obay.cloud>
2021-05-26 21:33:45 +0300

    Brew formula update for rcmt version v0.17.0

--- rcmt.rb
+++ rcmt.rb
@@ -5,24 +5,24 @@
 class Rcmt < Formula
   desc "A rudimentary configuration management tool for Slack Challenge."
   homepage "https://Github.com/obay/rcmt"
-  version "0.16.0"
+  version "0.17.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "http://github.com/obay/rcmt/releases/download/v0.16.0/rcmt_0.16.0_Darwin_x86_64.tar.gz"
-    sha256 "11c06f3b4b5eff1d8c574519fd9c6d1d3fec208f5674e371499430a7e8827582"
+    url "http://github.com/obay/rcmt/releases/download/v0.17.0/rcmt_0.17.0_Darwin_x86_64.tar.gz"
+    sha256 "edc22fad762b03ae44651c5ccfac03e9bddc64c70d1d24248eab18925eeeb938"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "http://github.com/obay/rcmt/releases/download/v0.16.0/rcmt_0.16.0_Darwin_arm64.tar.gz"
-    sha256 "a51345f51b05c6d48250a6f7b5f461d7c23ccb7ad3d7db35c57626a727664e60"
+    url "http://github.com/obay/rcmt/releases/download/v0.17.0/rcmt_0.17.0_Darwin_arm64.tar.gz"
+    sha256 "b3f8c03aae63a086334fb1ee707971e09a6b15a21e6feb158d7394388e652b9b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "http://github.com/obay/rcmt/releases/download/v0.16.0/rcmt_0.16.0_Linux_x86_64.tar.gz"
-    sha256 "525cb17601d2ab3c75a0102cc96561a7ebcac1eb188a7eb8d48176bc374c2cae"
+    url "http://github.com/obay/rcmt/releases/download/v0.17.0/rcmt_0.17.0_Linux_x86_64.tar.gz"
+    sha256 "b6f455aae149e37a94ec49bc6d6c85d70d427f42c49daf943289612352ef1125"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "http://github.com/obay/rcmt/releases/download/v0.16.0/rcmt_0.16.0_Linux_arm64.tar.gz"
-    sha256 "829f2f88535809514ff3764c5f4820e1040df70364a1f504386af0ff5263d423"
+    url "http://github.com/obay/rcmt/releases/download/v0.17.0/rcmt_0.17.0_Linux_arm64.tar.gz"
+    sha256 "c8c8b58409c74bcee2fc9f250cd96f610103c5a3409f6f94f0e919baae34bef1"
   end
 
   depends_on "go" => :build
