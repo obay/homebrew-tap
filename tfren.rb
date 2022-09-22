5e3d150b0b9f1898ee6dd74e5fdd25ebdc890966
goreleaserbot <goreleaser@obay.cloud>
2022-09-22 12:23:20 -0700

    Brew formula update for tfren version v0.1.17

--- tfren.rb
+++ tfren.rb
@@ -5,22 +5,22 @@
 class Tfren < Formula
   desc "A tool to rename Terraform files accodrind to the resource type and name."
   homepage "https://github.com/obay/tfren"
-  version "0.1.16"
+  version "0.1.17"
 
   depends_on "go" => :build
 
   on_macos do
     if Hardware::CPU.arm?
-      url "http://github.com/obay/tfren/releases/download/v0.1.16/tfren_0.1.16_Darwin_arm64.tar.gz"
-      sha256 "aa08ec5a5f9b28a6817eb9a5660a5e49db0bd70051ce1212ed602c8033808abe"
+      url "http://github.com/obay/tfren/releases/download/v0.1.17/tfren_0.1.17_Darwin_arm64.tar.gz"
+      sha256 "cd877cc25e9811e6369bbb5657fd5c90d4d97b88a937306110088c5a7e57fc5c"
 
       def install
         bin.install "tfren"
       end
     end
     if Hardware::CPU.intel?
-      url "http://github.com/obay/tfren/releases/download/v0.1.16/tfren_0.1.16_Darwin_x86_64.tar.gz"
-      sha256 "c0807f44e17956f61c594222e84bbd98acffc1a07f860706a24a84af135e6130"
+      url "http://github.com/obay/tfren/releases/download/v0.1.17/tfren_0.1.17_Darwin_x86_64.tar.gz"
+      sha256 "5513731b637f49d6484b6ffe9e56bca90ea927f016390037cf320ed2b26d8e00"
 
       def install
         bin.install "tfren"
@@ -30,16 +30,16 @@ class Tfren < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "http://github.com/obay/tfren/releases/download/v0.1.16/tfren_0.1.16_Linux_arm64.tar.gz"
-      sha256 "2d2ab369b430dade1e413d0c972e9b648713d9453389f442b56b8c5236bf3869"
+      url "http://github.com/obay/tfren/releases/download/v0.1.17/tfren_0.1.17_Linux_arm64.tar.gz"
+      sha256 "13c3ecef6220888399f62b59fbf2e42e906432f548c97b9708b8b6f4f56a5e70"
 
       def install
         bin.install "tfren"
       end
     end
     if Hardware::CPU.intel?
-      url "http://github.com/obay/tfren/releases/download/v0.1.16/tfren_0.1.16_Linux_x86_64.tar.gz"
-      sha256 "3371c467c51ec9e1eab56216edbc5a6a624b05e7cdb788b7d4a9195ee2d3f02d"
+      url "http://github.com/obay/tfren/releases/download/v0.1.17/tfren_0.1.17_Linux_x86_64.tar.gz"
+      sha256 "c2b396f1699e610a8ac3447279a363b40b71dc55be255d7f15274d94f86ab3fa"
 
       def install
         bin.install "tfren"
